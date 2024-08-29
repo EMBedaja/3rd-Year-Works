b992fef972509f346ff348a11e3f7a9c68d0c5a2
EMBedaja <100077814+EMBedaja@users.noreply.github.com>
2024-08-29 20:16:27 +0800

    Add files via upload

---
+++ PORTFOLIO/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EMB PORTFOLIO</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Island+Moments&family=Jomolhari&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+    <div class="header-content">
+            <div class="header-text">
+                <h1>EM.<span>B</span></h1>
+            </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Education</a></li>
+                <li><a href="#">Works</a></li>
+                <li><a href="#">Contact </a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+       
+        <!-- Add more content here -->
+    </main>
+    
+    <footer>
+        <p>&copy; 2024 EM.Bedaja</p>
+    </footer>
+</body>
+</html>
